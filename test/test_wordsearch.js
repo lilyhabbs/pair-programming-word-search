aab265bd960a482c72159dfa84e9ca00f6ea251a
Lily Habberfield <lilyhabbs@gmail.com>
2021-04-07 22:51:14 +0000

    Updated functions and created additional test cases

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,12 +15,12 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present horizonatally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -31,8 +31,30 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
 
     assert.isTrue(result);
+  });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'V', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'A', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'N', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'C', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'O', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'U', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'V', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'E', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'R', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'VANCOUVER');
+
+    assert.isTrue(result);
+  });
+
+  it("should return a message if the word matrix is an empty array", function() {
+    const actual = wordSearch([], 'WORD');
+    const expected = 'The word matrix is empty.';
+    assert.strictEqual(actual, expected);
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,39 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = function(matrix) {
+  const newMatrix = [];
 
-module.exports = wordSearch
+  for (let col = 0; col < matrix[0].length; col++) {
+    newMatrix[col] = [];
+    for (let row = 0; row < matrix.length; row++) {
+      newMatrix[col].push(matrix[row][col]);
+    }
+  }
+  return newMatrix;
+};
+
+
+const wordSearch = (letters, word) => {
+  if (letters.length === 0) {
+    return 'The word matrix is empty.';
+  }
+
+  // check for horizonal words
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+
+  // check for vertical words
+  const transposedMatrix = transpose(letters);
+  const verticalJoin = transposedMatrix.map(ls => ls.join(''));
+  for (const l of verticalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
